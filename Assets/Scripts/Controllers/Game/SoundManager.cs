8831af92bca79b6893dc89d8c16dbf42b124d8ee
Quang Khuat Tien <quang.khuat@ntq-solution.com.vn>
2023-01-29 14:43:12 +0700

    Coding for audio

--- Assets/Scripts/Controllers/Game/SoundManager.cs
+++ Assets/Scripts/Controllers/Game/SoundManager.cs
@@ -1,6 +1,7 @@
 namespace Controllers.Game
 {
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class SoundManager : MonoBehaviour
     {
@@ -9,9 +10,36 @@ namespace Controllers.Game
         [SerializeField] private AudioClip   backgroundAudio;
         [SerializeField] private AudioSource backgroundAudioSource;
         [SerializeField] private AudioSource sfxAudioSource;
+        [SerializeField] public Slider volumeSlier;
+        [SerializeField] public Image soundOnIcon;
+        [SerializeField] public Image soundOffIcon;
 
         public static SoundManager Instance;
+        private bool muted = false;
+        private void Start()
+        {
+            if (!PlayerPrefs.HasKey("musicVolume"))
+            {
+                PlayerPrefs.SetFloat("musicVolume", 1);
+                Load();
+            }
+            else
+            {
+                Load();
+            }
 
+            if (!PlayerPrefs.HasKey("muted"))
+            {
+                PlayerPrefs.SetInt("muted", 0);
+                LoadSound();
+            }
+            else
+            {
+                LoadSound();
+            }
+            UpdateButtonIcon();
+            AudioListener.pause = muted;
+        }
         private void Awake()
         {
             Instance                    = this;
@@ -31,6 +59,59 @@ namespace Controllers.Game
                     this.sfxAudioSource.Play();
                     break;
             }
+        }
+        public void ChangeVolume()
+        {
+            AudioListener.volume = volumeSlier.value;
+        }
+
+        private void Load()
+        {
+            volumeSlier.value = PlayerPrefs.GetFloat("musicVolume");
+        }
+
+        private void LoadSound()
+        {
+            muted = PlayerPrefs.GetInt("muted") == 1;
+        }
+
+        private void SaveVolume()
+        {
+            PlayerPrefs.SetFloat("musicVolume", volumeSlier.value);
+        }
+
+        private void SaveSound()
+        {
+            PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+        }
+        public void OnButtonPress()
+        {
+            if(muted == false)
+            {
+                muted = true;
+                AudioListener.pause = true;
+            }
+            else
+            {
+                muted = false;
+                AudioListener.pause = false;
+            }
+            SaveSound();
+            UpdateButtonIcon();
+        }
+
+        private void UpdateButtonIcon()
+        {
+            if(muted == false)
+            {
+                soundOnIcon.enabled = true;
+                soundOffIcon.enabled = false;
+            }
+            else
+            {
+                soundOnIcon.enabled = false;
+                soundOffIcon.enabled = true;
+            }  
         }
     }
 }
